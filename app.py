fba549d69fa51ea3c31c5a3a49843b6502ab6c07
Gurudeeban Selvaraj <42083429+Gurudeeban-Selvaraj@users.noreply.github.com>
2021-09-16 11:25:34 -0400

    Update app.py

--- app.py
+++ app.py
@@ -18,6 +18,17 @@ import os
 import pybase64
 import pickleshare
 
+# Create a page dropdown 
+page = st.selectbox("Choose your page", ["Page 1", "Page 2", "Page 3"]) 
+if page == "Page 1":
+    # Display details of page 1
+elif page == "Page 2":
+    # Display details of page 2
+elif page == "Page 3":
+    # Display details of page 3
+
+
+
 # Molecular descriptor calculator
 def desc_calc():
     # Performs the descriptor calculation
